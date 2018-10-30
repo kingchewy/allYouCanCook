d52b3712ba01143b6ab09a9e6d1afc824b1814a0
kingchewy <32060652+kingchewy@users.noreply.github.com>
2018-10-30 08:21:38 +0100

    Delete credentials.ts

--- src/app/credentials.ts
+++
@@ -1,9 +0,0 @@
-
-export const firebaseConfig = {
-    apiKey: "AIzaSyC356eJpQ4m5OgR7BVPOPMvyRteFkREs30",
-    authDomain: "allyoucancook-10305.firebaseapp.com",
-    databaseURL: "https://allyoucancook-10305.firebaseio.com",
-    projectId: "allyoucancook-10305",
-    storageBucket: "allyoucancook-10305.appspot.com",
-    messagingSenderId: "477607491181"
-  };
